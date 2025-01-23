81403a3156751037b50d1f81de4e79d3a6f05713
Amit Trivedi <amittrivedi@Amits-MacBook-Air.local>
2025-01-23 21:01:30 +0530

    Discount tag added

--- src/components/grid/item-card.jsx
+++ src/components/grid/item-card.jsx
@@ -1,8 +1,20 @@
 const ItemCard = ({product}) => {
     const {name, category, price, discounted_price, image} = product;
     return (
-        <div className="item-card d-flex flex-column mt-5" style={{width:"285px",height:"420px", backgroundColor:"whitesmoke"}}>
-            <div className="item-image"><img style={{width:"100%",height:"300px", objectFit: 'cover'}} src={`/${image.replace('./', '')}`} alt={name} /></div>
+        <div className="item-card d-flex flex-column mt-5" style={{width:"285px",height:"420px", backgroundColor:"whitesmoke",overflow: "hidden",position: "relative"}}>
+            <div className="item-image"><img style={{width:"100%",height:"300px", objectFit: 'cover'}} src={`/${image.replace('./', '')}`} alt={name} />
+                {
+                    discounted_price == null && (
+                        <div style={{width :"50px", height : "50px",backgroundColor :"#2EC1AC",borderRadius :"50%",textAlign : "center",justifyContent:"center",display : "flex",alignItems:"center", position: "absolute",top:"24px",left:"213px"}}>New</div>
+                    )
+                }
+
+                {
+                    discounted_price != null && (
+                        <div style={{width :"50px", height : "50px",backgroundColor :"#E97171",borderRadius :"50%",textAlign : "center",justifyContent:"center",display : "flex",alignItems:"center", position: "absolute",top:"24px",left:"213px"}}>50%</div>
+                    )
+                }
+            </div>
             <div className="container mt-2">
                 <div className="item-title"><b style={{fontSize:"24px"}}>{name}</b></div>
                 <div className="item-category" style={{color:"gray"}}>{category}</div>
