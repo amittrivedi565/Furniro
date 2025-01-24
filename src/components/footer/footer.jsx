d671d33c084844422b9f77ca0f23bb02b066e1f4
Amit Trivedi <amittrivedi@Amits-MacBook-Air.local>
2025-01-24 17:16:23 +0530

    Footer reponsive added

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,46 +6,46 @@ import { ReactComponent as SupportIcon } from "../../assets/svgs/footer/support.
 export default function FeatureFooter() {
   return (
     <div
-      className="container-fluid mt-5 footer-class d-flex justify-content-center align-items-center flex-md-column"
-      style={{ height: "250px", backgroundColor: "#FAF3EA" }}
+      className="container-fluid mt-5 footer-class d-flex justify-content-center align-items-center"
+      style={{ backgroundColor: "#FAF3EA", padding: "20px 0",minHeight:"250px" }}
     >
-      <div className="row d-flex justify-content-center align-items-center w-100">
-        <div className="col-3 d-flex flex-column justify-content-center align-items-center text-center">
+      <div className="row w-100 text-center">
+        <div className="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center mb-4 mb-md-0">
           <TrophyIcon />
           <span className="mt-2">
-            <b style={{ fontSize: "25px", fontWeight: "600" }}>High Quality</b>
+            <b style={{ fontSize: "20px", fontWeight: "600" }}>High Quality</b>
             <br />
-            <span style={{ fontSize: "20px", fontWeight: "500", color: "gray" }}>
-              crafted from top materials
+            <span style={{ fontSize: "16px", color: "gray" }}>
+              Crafted from top materials
             </span>
           </span>
         </div>
-        <div className="col-3 d-flex flex-column justify-content-center align-items-center text-center">
+        <div className="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center mb-4 mb-md-0">
           <CheckIcon />
           <span className="mt-2">
-            <b style={{ fontSize: "25px", fontWeight: "600" }}>Warranty Protection</b>
+            <b style={{ fontSize: "20px", fontWeight: "600" }}>Warranty Protection</b>
             <br />
-            <span style={{ fontSize: "20px", fontWeight: "500", color: "gray" }}>
+            <span style={{ fontSize: "16px", color: "gray" }}>
               Over 2 years
             </span>
           </span>
         </div>
-        <div className="col-3 d-flex flex-column justify-content-center align-items-center text-center">
+        <div className="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center mb-4 mb-md-0">
           <ShippingIcon />
           <span className="mt-2">
-            <b style={{ fontSize: "25px", fontWeight: "600" }}>Free Shipping</b>
+            <b style={{ fontSize: "20px", fontWeight: "600" }}>Free Shipping</b>
             <br />
-            <span style={{ fontSize: "20px", fontWeight: "500", color: "gray" }}>
+            <span style={{ fontSize: "16px", color: "gray" }}>
               Order over $150
             </span>
           </span>
         </div>
-        <div className="col-3 d-flex flex-column justify-content-center align-items-center text-center">
+        <div className="col-12 col-md-3 d-flex flex-column justify-content-center align-items-center">
           <SupportIcon />
           <span className="mt-2">
-            <b style={{ fontSize: "25px", fontWeight: "600" }}>24 / 7 Support</b>
+            <b style={{ fontSize: "20px", fontWeight: "600" }}>24 / 7 Support</b>
             <br />
-            <span style={{ fontSize: "20px", fontWeight: "500", color: "gray" }}>
+            <span style={{ fontSize: "16px", color: "gray" }}>
               Dedicated support
             </span>
           </span>
